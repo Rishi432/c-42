cd35c0a032d3afc0edbc34e4f429ee5ec72a3778
Rishi432 <80186878+Rishi432@users.noreply.github.com>
2021-06-26 09:40:30 +0530

    Add files via upload

---
+++ game obstacle/sketch.js
@@ -0,0 +1,69 @@
+var canvas, backgroundImage;
+
+var gameState = 0,finishedPlayers;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var track, car1_img, car2_img, car3_img, car4_img;
+var f1 , f1img;
+var obstacle
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  f1img = loadImage("images/f1.png");
+
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth , displayHeight);
+  database = firebase.database();
+  gameState = 0;
+  distance = 0;
+ // finishedPlayers = 0;
+  yVel = 0;
+  xVel = 0;
+  obstacle= createGroup();
+ for(var i=0; i < 5 ; i++){
+  A = random (200,900);
+  B = random (-height*4,height - 300);
+  f1 = createSprite(A,B)
+  f1.addImage("f1",f1img);
+  obstacle.add(f1);
+ }
+  xSet = false;
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+}
+
+
+function draw(){
+   //start the game
+   background(200, 200, 255);
+
+   //start the game
+   if (playerCount === 4 ) {
+     game.update(1);
+   }
+ 
+   //start the game for real
+   if (gameState === 1) {
+     game.play();
+   }
+   if (gameState === 2) {
+     console.log("End");
+   }}
+ 
+  
+  
